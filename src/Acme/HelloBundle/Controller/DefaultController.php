897584b3520c99c824803a4ccd252c2097fba865
e.andreev <you@example.com>
2017-05-18 22:47:18 +0300

    first symfony project

---
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AcmeHelloBundle:Default:index.html.twig');
+    }
+    
+    public function helloAction($name){
+        
+        return $this->render('AcmeHelloBundle:Default:hello.html.twig', [
+            "name" => $name,
+        ]);
+    }
+}
